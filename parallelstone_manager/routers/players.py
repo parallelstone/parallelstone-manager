a1ea2112516b44b08c903e2eb0b34ba481c6cede
GWANWOO KIM <gwk1125@hanyang.ac.kr>
2025-07-16 11:47:00 +0900

    Update players.py
    Refactored player-related endpoints to use the RCONService for real-time data and actions.

--- parallelstone_manager/routers/players.py
+++ parallelstone_manager/routers/players.py
@@ -1,17 +1,46 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from parallelstone_manager.core.dependencies import get_rcon_service, RCONService
+from parallelstone_manager.services import rcon
 
 router = APIRouter()
 
 @router.get("/") 
-def get_players():
-    return {"status": "running", "player_count": 5}
+async def get_players(rcon: RCONService = Depends(get_rcon_service)):
+    try:
+        result = await rcon.execute_command("list")
+        num_of_players = result.split()[2]
+    except Exception as e:
+        return {"status": "error", "error": str(e)}
 
-@router.get("/count") 
-def get_players_count():
-    return {"status": "running", "player_count": 5}
+    return {"status": "running", "player_count": num_of_players}
 
-@router.post("/kick/{player_name}") 
-def kick_player():
-    # Kick player
-    return {"status": "running", "player_count": 5}
+@router.get("/player_list")
+async def get_player_list(rcon: RCONService = Depends(get_rcon_service)):
+    try:
+        result = await rcon.execute_command("list")
+        lst = result.split()
+        lst = lst[10:]
+    except Exception as e:
+        return {"status": "error", "error": str(e)}
+
+    return {"status": "running", "players": lst}
+
+@router.get("/count")
+async def get_players_count():
+    try:
+        result = await rcon.execute_command("list")
+        num_of_players = result.split()[2]
+    except Exception as e:
+        return {"status": "error", "error": str(e)}
+
+    return {"status": "running", "player_count": num_of_players}
+
+@router.post("/kick")
+async def kick_player(player_name: str, rcon: RCONService = Depends(get_rcon_service)):
+    try:
+        result = await rcon.execute_command(f"kick {player_name}")
+    except Exception as e:
+        return {"status": "error", "error": str(e)}
+
+    return {"status": "running", "msg": result}
 
